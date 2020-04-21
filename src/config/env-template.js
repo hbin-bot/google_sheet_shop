6200a6eaaa1b6d7dbfd7f7acd89578833335f1c3
Bin <hbin1990@hotmal.com>
2020-04-21 09:22:47 -0500

    Untrack env.js and added env_template.js

---
+++ src/config/env-template.js
@@ -0,0 +1,6 @@
+export const googleParams = {
+	sheets_api : "https://sheets.googleapis.com/v4/spreadsheets/",
+	api_key : "AIzaSyD2pB4sHe8F_1NljQVueea_j2eWqn3CKTs",
+	layout_id : "1bat4EeidRdT5y2HT2OMR_DY3TQBoRbtvzFuBLyFgwjs",
+	product_id : "1UADDol15F1FToA3hR4V-CdpRJV7pG0HFu7U7pCDTMD0"
+};
